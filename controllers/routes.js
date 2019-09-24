f2726476858532b7169cd142ab12191ff48865ef
akaryrye <ralldrin@gmail.com>
2019-09-23 18:26:25 -0700

    seed db w/ demo data + render to template

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,11 +1,26 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  
   // Render index page
   app.get("/", function(req, res) {
-    let object = {
-      hello: "Hello Boilerplate"
-    }
-    res.render("index", object);
+    db.Model.findOne({where:{id:1}
+      }).then(function(result) {
+        if (result) {
+          console.log(result.dataValues.row01);
+          res.render("index", result.dataValues);
+        } else {
+          db.Model.create({
+            row01: "Hello",
+            row02: "World"
+          }, {
+            where: {id: 1}
+          }).then( function(data) {
+            console.log(data[0]);
+            res.render("index", data.dataValues);
+          });
+        }
+      });
   });
+
 };
